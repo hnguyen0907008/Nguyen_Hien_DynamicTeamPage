03033964dc060eef7924fd26d8a8562bb25d9ab0
hnguyen0907008 <43183034+hnguyen0907008@users.noreply.github.com>
2020-04-04 08:51:01 -0400

    add js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,37 @@
 
 	const name = document.querySelector(".memberInfo h2"),
 		  contents = document.querySelector(".detail"),
+		  position = document.querySelector(".memberInfo h3"),
 		  buttons = document.querySelectorAll(".controlImg button"),
-		  memberImg =  document.querySelector(".imageCon");
-		  des = document.querySelector("#hien");
+		  memberImg =  document.querySelector(".imageCon"),
+		  desicons = document.querySelector("#desIcons"),
+		  des = document.querySelector("#hien"),
 		  dev = document.querySelector("#other");
 
 	const twoMembers = [
-		["Hien Nguyen", `Info 1: orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam faucibus purus in massa tempor nec. Quam id leo in vitae turpis massa sed elementum tempus. Magna eget est lorem ipsum. Sed odio morbi quis commodo odio aenean. Egestas congue quisque egestas diam in arcu cursus euismod quis. Condimentum lacinia quis vel eros donec ac odio tempor orci. Urna nec tincidunt praesent semper feugiat nibh. Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Enim neque volutpat ac tincidunt vitae. Nibh nisl condimentum id venenatis. Risus ultricies tristique nulla aliquet enim tortor at auctor. Arcu vitae elementum curabitur vitae nunc sed. Sed augue lacus viverra vitae congue. Viverra nibh cras pulvinar mattis nunc sed blandit. Sagittis purus sit amet volutpat. Ac ut consequat semper viverra. Nullam non nisi est sit amet. Consectetur a erat nam at lectus urna duis.`, `images/portrait_bw.jpg`], 
+		["Hien Nguyen", `I am aspiring graphic designer and brandeveloper based in London, Ontario, Canada.
+		For me, designing is solving visual problems. I work with my team in Fanshawe College to help businesses' visualize their brand identity and reach out to their target audience in the smoothest way. To achieve these, I focus on working closely with teammates to find the best marketing strategy solutions.
+		Aside from graphic design, I have expertise in illustration and motion graphic.
+		Tools frequently used in my projects:`, `Designer`], 
 
-		["The Other Person", `Info 2: Sed felis eget velit aliquet sagittis id consectetur purus. Felis donec et odio pellentesque. Sed libero enim sed faucibus turpis in eu mi bibendum. Imperdiet sed euismod nisi porta lorem mollis aliquam. Egestas pretium aenean pharetra magna. Semper eget duis at tellus at urna condimentum mattis pellentesque. Turpis massa tincidunt dui ut ornare. Viverra aliquet eget sit amet tellus cras adipiscing enim. Natoque penatibus et magnis dis parturient montes nascetur ridiculus. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Duis ut diam quam nulla. Mi proin sed libero enim sed.`, `images/portrait2_bw.jpg`]
+		["The Other Person", `This is information of the developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`, `Developer`]
 		];
 
 	function showBioInfo(){
 		arrayIndex = this.dataset.arrayref;
 		name.textContent = twoMembers[arrayIndex][0];
 		contents.textContent = twoMembers[arrayIndex][1];
+		position.textContent = twoMembers[arrayIndex][2];
 	}
 
 	function changeImageDes(){
-		document.getElementById('defaultImg').src = "images/portrait_bw.jpg";
+		document.getElementById('defaultImg').src = "images/portrait_1.png";
+		desicons.style.visibility='visible';
 	}
 
 	function changeImageDev(){
-		document.getElementById('defaultImg').src = "images/portrait_bw2.jpg";
+		document.getElementById('defaultImg').src = "images/portrait_2.jpg";
+		desicons.style.visibility='hidden';
 	}
 
 	buttons.forEach(button => button.addEventListener("click", showBioInfo));
